c6020edecd892b1a85ac96f20a7d23be124cefc1
Minh-Nghia Nguyen <elvin.ss127@gmail.com>
2018-04-15 23:56:09 +0100

    Fix metrics and rerun some experiments

--- metrics.py
+++ metrics.py
@@ -5,17 +5,11 @@ from sklearn.metrics import (roc_auc_score, precision_score,
 
 def metrics(y_test, out_y):
     result = dict()
-    result['Confusion matrix'] = confusion_matrix(y_test, out_y, labels=[1, -1])
-    result['Precision'] = precision_score(y_test, out_y)
-    result['Recall'] = recall_score(y_test, out_y)
-    result['F1'] = f1_score(y_test, out_y)
-    result['AUROC'] = roc_auc_score(y_test, out_y)
-    result['AUPRC'] = average_precision_score(y_test, out_y)
-
-    idx = np.argsort(out_y)
-    predLabel = np.ones(y_test.shape)
-
-    predLabel[idx[:10]] = -1
-    result['P@10'] = precision_score(y_test, predLabel)
+    result['Confusion matrix'] = confusion_matrix(-y_test, -out_y, labels=[1, -1])
+    result['Precision'] = precision_score(-y_test, -out_y)
+    result['Recall'] = recall_score(-y_test, -out_y)
+    result['F1'] = f1_score(-y_test, -out_y)
+    result['AUROC'] = roc_auc_score(-y_test, -out_y)
+    result['AUPRC'] = average_precision_score(-y_test, -out_y)
 
     return result
